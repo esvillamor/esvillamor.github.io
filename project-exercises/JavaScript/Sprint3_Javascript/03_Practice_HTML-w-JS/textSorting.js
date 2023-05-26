1d8649ef76ff8b339a8afcdca3229a632b3546e2
esvillamor <eric.s.villamor@gmail.com>
2023-05-27 07:44:48 +0800

    TextSorting

---
+++ project-exercises/JavaScript/Sprint3_Javascript/03_Practice_HTML-w-JS/textSorting.js
@@ -0,0 +1,31 @@
+var words = [];
+
+        function addWord() {
+            var wordInput = document.getElementById("word-input");
+            var word = wordInput.value;
+            if (word !== "") {
+                words.push(word);
+                wordInput.value = "";
+                displayWords();
+            }
+        }
+
+        function sortWords() {
+            words.sort();
+            displayWords();
+        }
+
+        function unsortWords() {
+            words.reverse();
+            displayWords();
+        }
+
+        function displayWords() {
+            var wordList = document.getElementById("word-list");
+            wordList.innerHTML = "";
+            for (var i = 0; i < words.length; i++) {
+                var listItem = document.createElement("li");
+                listItem.appendChild(document.createTextNode(words[i]));
+                wordList.appendChild(listItem);
+            }
+        }
